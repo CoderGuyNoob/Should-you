7992862b499535473521373c7894a77182c2ba39
Vir Maiya <vir.maiya@gmail.com>
2026-02-09 18:44:18 -0500

    updating other

--- menu_adult.py
+++ menu_adult.py
@@ -15,7 +15,7 @@ menu = {
         "options": {
             "1": ("Yes", "0-1-yes"),
             "2": ("No", "0-1-no"),
-            "3": ("I don't know", "statement:Find out first")
+            "3": ("I don't know", "statement:Find out first|quit")
         }
     },
 
@@ -35,7 +35,7 @@ menu = {
         "options": {
             "1": ("Through friends", "0-1-no-inperson-friends"),
             "2": ("Through school", "0-1-no-inperson-school"),
-            "3": ("Third space (coffee shop, library, etc.)", "statement:Tell them, it's low risk")
+            "3": ("Third space (coffee shop, library, etc.)", "statement:Tell them, it's low risk|quit")
         }
     },
 
@@ -51,15 +51,15 @@ menu = {
     "0-1-no-inperson-friends-yes": {
         "prompt": "Are you in a friend group with them?",
         "options": {
-            "1": ("Yes", "statement:Tread carefully, could be awkward but tell them if you want clarity"),
-            "2": ("No", "statement:You should tell them, expect some awkwardness")
+            "1": ("Yes", "statement:Tread carefully, could be awkward but tell them if you want clarity|quit"),
+            "2": ("No", "statement:You should tell them, expect some awkwardness|quit")
         }
     },
 
     "0-1-no-inperson-friends-no": {
         "prompt": "Do you and your crush have a mutual friend?",
         "options": {
-            "1": ("Yes", "0-1-no-inperson-friends-yes"),  # shared menu
+            "1": ("Yes", "0-1-no-inperson-friends-yes"),
             "2": ("No, they are a friend’s sibling", "0-1-no-inperson-friends-sibling")
         }
     },
@@ -67,8 +67,8 @@ menu = {
     "0-1-no-inperson-friends-sibling": {
         "prompt": "Do you like them just because they're your friend's sibling?",
         "options": {
-            "1": ("Yes", "statement:Don't tell them, you'll phase out of it"),
-            "2": ("No", "statement:Tell them, but be prepared for awkwardness with your friend")
+            "1": ("Yes", "statement:Don't tell them, you'll phase out of it|quit"),
+            "2": ("No", "statement:Tell them, but be prepared for awkwardness with your friend|quit")
         }
     },
 
@@ -77,15 +77,15 @@ menu = {
         "prompt": "Do you see your crush often at school?",
         "options": {
             "1": ("Yes", "0-1-no-inperson-school-yes"),
-            "2": ("No", "statement:Tell them, nothing to risk")
+            "2": ("No", "statement:Tell them, nothing to risk|quit")
         }
     },
 
     "0-1-no-inperson-school-yes": {
         "prompt": "Do you like them because of proximity or genuinely?",
         "options": {
-            "1": ("Only because of forced proximity", "statement:Don't tell them"),
-            "2": ("Genuine feelings", "statement:Tell them, best case you succeed, worst case awkward")
+            "1": ("Only because of forced proximity", "statement:Don't tell them|quit"),
+            "2": ("Genuine feelings", "statement:Tell them, best case you succeed, worst case awkward|quit")
         }
     },
 
@@ -93,7 +93,7 @@ menu = {
     "0-1-no-online": {
         "prompt": "Is your crush a celebrity?",
         "options": {
-            "1": ("Yes", "statement:You can tell them, but they won't notice"),
+            "1": ("Yes", "statement:You can tell them, but they won't notice|quit"),
             "2": ("No", "0-1-no-online-real")
         }
     },
@@ -101,7 +101,7 @@ menu = {
     "0-1-no-online-real": {
         "prompt": "Did you meet them on a dating app?",
         "options": {
-            "1": ("Yes", "statement:Tell them, they probably know"),
+            "1": ("Yes", "statement:Tell them, they probably know|quit"),
             "2": ("No", "0-1-no-online-meet")
         }
     },
@@ -109,8 +109,8 @@ menu = {
     "0-1-no-online-meet": {
         "prompt": "Have you met them in real life?",
         "options": {
-            "1": ("Yes", "statement:Tell them"),
-            "2": ("No", "statement:You can tell them, but be prepared that reality may differ")
+            "1": ("Yes", "statement:Tell them|quit"),
+            "2": ("No", "statement:You can tell them, but be prepared that reality may differ|quit")
         }
     },
 
@@ -118,8 +118,8 @@ menu = {
     "0-1-no-work": {
         "prompt": "Do you fear being fired if you tell them?",
         "options": {
-            "1": ("Yes", "statement:Don't tell them"),
-            "2": ("No", "statement:You could tell them, proceed carefully")
+            "1": ("Yes", "statement:Don't tell them|quit"),
+            "2": ("No", "statement:You could tell them, proceed carefully|quit")
         }
     },
 
@@ -129,7 +129,7 @@ menu = {
         "options": {
             "1": ("Hooking up", "0-1-yes-hookup"),
             "2": ("Casually dating", "0-1-yes-casual"),
-            "3": ("In a serious relationship", "statement:Don't tell them, homewrecker")
+            "3": ("In a serious relationship", "statement:Don't tell them, homewrecker|quit")
         }
     },
 
@@ -137,8 +137,8 @@ menu = {
     "0-1-yes-casual": {
         "prompt": "Is it actually casual and with one person?",
         "options": {
-            "1": ("Yes", "statement:Proceed with caution, might work"),
-            "2": ("No", "statement:Don't tell them, avoid complications")
+            "1": ("Yes", "statement:Proceed with caution, might work|quit"),
+            "2": ("No", "statement:Don't tell them, avoid complications|quit")
         }
     },
 
@@ -146,8 +146,8 @@ menu = {
     "0-1-yes-hookup": {
         "prompt": "Are they in a situationship? Who controls it?",
         "options": {
-            "1": ("They are being dragged along", "statement:Tell them, but likely won't reciprocate"),
-            "2": ("They are dragging the other person", "statement:Don't tell them")
+            "1": ("They are being dragged along", "statement:Tell them, but likely won't reciprocate|quit"),
+            "2": ("They are dragging the other person", "statement:Don't tell them|quit")
         }
     }
 }
